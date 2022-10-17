44f3f8c164c28aedef87f5675c7d44cc04a7b4c3
jpope229 <jpope229@mtroyal.ca>
2022-10-17 02:01:46 -0600

    adding nav

---
+++ favorites.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Favorites page</title>
+</head>
+
+<body>
+    <header>
+        <div class="nav">
+            <a href="index.php">Home</a>
+            <a href="search.php">Search</a>
+            <a href="browse.php">Browse</a>
+            <a href="favorites.php">Favorites</a>
+        </div>
+
+        <h2>COMP 3512 ASG1<h2>
+                <p>Justin Pope</p>
+
+    </header>
+
+    <h1>Favorites</h1>
+
+
+    <footer>FOOTER</footer>
+</body>
+
+</html>
