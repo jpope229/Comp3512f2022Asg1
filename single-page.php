beda5b47f99caae5ad1ac80479c81f0077438771
jpope229 <jpope229@mtroyal.ca>
2022-10-16 23:35:57 -0600

    creating new files

---
+++ single-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Single song</title>
+    <link rel="stylesheet" href="css/single-page.css">
+</head>
+
+<body>
+    <header>HEADER</header>
+    <h1>Song information</h1>
+
+
+
+
+    <footer>FOOTER</footer>
+</body>
+
+</html>
